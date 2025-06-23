f47fee791df39746b382aceee737b74e81cc023a
sourakdev <souravsingh16299@gmail.com>
2025-06-23 16:14:20 +0530

    commit added mutate async & minor changes

--- new_frontend/proxyOptions.ts
+++ new_frontend/proxyOptions.ts
@@ -1,20 +1,20 @@
-// import common_site_config from '../../../sites/common_site_config.json';
+import common_site_config from '../../../sites/common_site_config.json';
 
-// const { webserver_port } = common_site_config;
+const { webserver_port } = common_site_config;
 
 const uat = "https://uat.unityedu.tech";
 
 export default {
   '^/(app|api|assets|files|private)': {
-    // target: `http://127.0.0.1:${webserver_port}`,
-    target: uat,
+    target: `http://127.0.0.1:${webserver_port}`,
+    // target: uat,
     ws: true,
     changeOrigin: true,
     router: function (req: any) {
       const site_name = req.headers.host.split(':')[0];
       console.log(req)
-      // return `http://${site_name}:${webserver_port}`;
-      return uat;
+      return `http://${site_name}:${webserver_port}`;
+      // return uat;
     }
   }
 };
